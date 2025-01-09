4973cfad17da7c6f07490817854bcb8fd9559a64
jonathanhansson <jonathanhanssondg@gmail.com>
2025-01-09 14:07:58 +0100

    Simplified the Main()-method

--- Program.cs
+++ Program.cs
@@ -6,30 +6,9 @@ using System.Runtime.InteropServices;
 
 class Program
 {
-    string connectionString = File.ReadAllText("connectionstring.txt");
-    
     static void Main()
     {
-        ConnectionHandler connectionHandler = new ConnectionHandler();
         Menu menu = new Menu();
-
-        System.Console.WriteLine("1. Ändra info i databasen\n2. Hämta ut statistik\n3. Avsluta");
-        
-        int answer = int.Parse(Console.ReadLine());
-
-        switch (answer)
-        {
-            case 1:
-                menu.LetPlayerInsert();
-                break;
-
-            case 2:
-                connectionHandler.FetchSalesPersons();
-                break;
-
-            case 3:
-                connectionHandler.RankBySales();
-                break;
-        }     
+        menu.DisplayMainMenu();
     }
 }
