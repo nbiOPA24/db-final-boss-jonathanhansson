2ea8a98a85feb2490780da0dab889258fb629db1
jonathanhansson <jonathanhanssondg@gmail.com>
2025-01-09 14:05:34 +0100

    Added a method that displays the main menu

--- Classes/MenuClass.cs
+++ Classes/MenuClass.cs
@@ -3,6 +3,42 @@ using System.Net;
 public class Menu
 {
     UserInterface ui = new UserInterface();
+    ConnectionHandler connectionHandler = new ConnectionHandler();
+
+    public void DisplayMainMenu()
+    {
+        while (true)
+        {
+            Menu menu = new Menu();
+
+            System.Console.WriteLine("1. Ändra info i databasen\n2. Hämta ut statistik\n3. Ranka säljare efter antal ordrar\n4. Visa alla blabla\n5. Avsluta");
+
+            int answer = int.Parse(Console.ReadLine());
+
+            switch (answer)
+            {
+                case 1:
+                    menu.LetPlayerInsert();
+                    break;
+
+                case 2:
+                    ui.DisplayCustomerWithAboveAverageSpending();
+                    break;
+
+                case 3:
+                    ui.DisplaySalesPersonsRankedAfterSales();
+                    break;
+
+                case 4:
+                    ui.DisplayProducts();
+                    break;
+                
+                case 5:
+                    Environment.Exit(0);
+                    return;
+            }
+        }
+    }
     public void LetPlayerInsert()
     {
         while (true)
