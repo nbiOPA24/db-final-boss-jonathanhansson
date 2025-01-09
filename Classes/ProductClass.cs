f3017ddd5e3a709914281c44511a13cd65532a41
jonathanhansson <jonathanhanssondg@gmail.com>
2025-01-09 12:00:15 +0100

    Removed the Interface heritage from the product-class

--- Classes/ProductClass.cs
+++ Classes/ProductClass.cs
@@ -1,6 +1,6 @@
-public class Product : IDiscountable
+public class Product
 {
-    public int Id { get;set; }
+    public int Id { get; set; }
     public string Name { get; set; }
     public int Stock { get; set; }
     public float Price { get; set; }
