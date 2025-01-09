ae3686e0d604b7b7c2d7a006952d6016a2330c44
jonathanhansson <jonathanhanssondg@gmail.com>
2025-01-09 14:01:03 +0100

    Added polymorphism to display how a Customer is displayed in the UI

--- Classes/CustomerClass.cs
+++ Classes/CustomerClass.cs
@@ -1,4 +1,10 @@
 public class Customer
 {
+    public string Name {get;set;}
+    public decimal TotalRevenue {get;set;}
 
+    public override string ToString()
+    {
+        return $"Name: {Name}, Total revenue: {TotalRevenue}";
+    }
 }
